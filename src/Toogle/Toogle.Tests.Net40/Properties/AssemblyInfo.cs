87c6b5fe91f03a0fa423c2f6e3d49eace3c28d81
Kris McGinnes <kmcginnes@me.com>
2015-07-20 11:04:01 -0500

    Added test project for net 40

--- src/Toogle/Toogle.Tests.Net40/Properties/AssemblyInfo.cs
+++ src/Toogle/Toogle.Tests.Net40/Properties/AssemblyInfo.cs
@@ -1,20 +1,26 @@
-﻿using System.Resources;
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SpicyTaco.Toogle")]
-[assembly: AssemblyDescription("Feature toggling for mere mortals")]
+[assembly: AssemblyTitle("SpicyTaco.Toogle.Tests.Net40")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("SpicyTaco")]
-[assembly: AssemblyProduct("Toogle")]
+[assembly: AssemblyProduct("Toogle.Tests.Net40")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: NeutralResourcesLanguage("en")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("db47fac0-6238-477d-94da-0a8c908fda0b")]
 
 // Version information for an assembly consists of the following four values:
 //
